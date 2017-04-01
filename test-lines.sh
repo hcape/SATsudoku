1d507fe3686b1a5f6e3c96f6e7d81e8ed14c0717
Brynn Hawker <brynn@hawker.me>
2017-03-31 20:07:36 -0700

    Tweak test lines

--- test-lines.sh
+++ test-lines.sh
@@ -6,16 +6,16 @@ if [ -z "$1" ]
     exit 1
 fi
 
-echo "Splitting $(basename $1)"
+printf "Testing $(basename $1)\n"
 
 mkdir -p tmp/$(basename $1).puzzles
 split -d -e -l 1 $1 tmp/$(basename $1).puzzles/
 
 mkdir -p out/$(basename $1).solns/
 for file in tmp/$(basename $1).puzzles/*; do
-  echo "Testing $file"
+  printf "\rTesting Puzzle #$(basename $file)"
   ./test.sh $file > out/$(basename $1).solns/$(basename $file).soln
 done
 
-echo "Writing solutions to out/$(basename $1).soln"
+printf "\nWriting solutions to out/$(basename $1).soln\n"
 cat out/$(basename $1).solns/* > out/$(basename $1).soln
