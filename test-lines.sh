6733eeae2a8166874014bb116a9a3eb7b7d56add
Brynn Hawker <brynn@hawker.me>
2017-03-31 20:01:24 -0700

    Make scripts more verbose

--- test-lines.sh
+++ test-lines.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-
 if [ -z "$1" ]
   then
     echo "Usage: $0 <multiline puzzle file>"
     exit 1
 fi
 
+echo "Splitting $(basename $1)"
 
 mkdir -p tmp/$(basename $1).puzzles
 split -d -e -l 1 $1 tmp/$(basename $1).puzzles/
 
 mkdir -p out/$(basename $1).solns/
 for file in tmp/$(basename $1).puzzles/*; do
+  echo "Testing $file"
   ./test.sh $file > out/$(basename $1).solns/$(basename $file).soln
 done
 
+echo "Writing solutions to out/$(basename $1).soln"
 cat out/$(basename $1).solns/* > out/$(basename $1).soln
--- test.sh
+++ test.sh
@@ -11,12 +11,25 @@ fi
 # Prep
 mkdir -p tmp
 
+echo
+echo "==== TESTING: $1 ===="
+
 # Convert to CNF in DIMACS
+echo
+echo "== TRANSLATING to DIMACS =="
+echo "python sud2sat.py $1 tmp/$(basename $1).cnf"
 python sud2sat.py $1 tmp/$(basename $1).cnf
 
 # Run SAT solver
+echo
+echo "== SOLVING: tmp/$(basename $1).cnf =="
+echo "minisat tmp/$(basename $1).cnf tmp/$(basename $1).sat"
+echo
 minisat tmp/$(basename $1).cnf tmp/$(basename $1).sat & wait
 
 # Print solved sudoku
+echo
+echo "== PARSING SOLUTION =="
+echo "python sat2sud.py tmp/$(basename $1).sat"
 python sat2sud.py tmp/$(basename $1).sat
 
